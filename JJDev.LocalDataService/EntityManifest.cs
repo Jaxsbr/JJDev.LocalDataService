6045f87ec58bfa6e677f2998842185b4e2b0bb05
Jaxsbr <19993683+Jaxsbr@users.noreply.github.com>
2024-03-29 20:19:59 +0000

    Added EntityManifestRecord file

--- JJDev.LocalDataService/EntityManifest.cs
+++ JJDev.LocalDataService/EntityManifest.cs
@@ -1,12 +1,35 @@
 ﻿namespace JJDev.LocalDataService
 {
+    /// <summary>
+    /// EntityManifest represents a physical file on the local computer.
+    /// The file contains a record of the owner and a collection of entity records for
+    /// each entity currently stored on the local computer.
+    /// </summary>
     public class EntityManifest
     {
         public string OwnerId { get; private set; }
 
+        private List<EntityManifestRecord> _records;
+
         public EntityManifest(string ownerId)
         {
             OwnerId = ownerId;
+            _records = new List<EntityManifestRecord>();
         }
     }
+
+    internal struct EntityManifestRecord
+    {
+        internal string Id { get; set; }
+
+        internal string EntityType { get; set; }
+
+        internal EntityManifestRecord(string id, string entityType)
+        {
+            Id = id;
+            EntityType = entityType;
+        }
+    }
+
+
 }
